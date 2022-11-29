608d05ef41bf738a4fe87dbfcb235a136ba0048e
Hirenkunar <hireneelpro@yahoo.com>
2022-11-28 23:09:34 -0500

    new component created

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./components/category-item/category-item.scss"
+
+import Categories from "./components/categories/categories";
+
+const App = () => {
+  return <Categories/>
+};
+
+export default App;
